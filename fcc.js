2bd2aa5209664b87cfbe9bb9812b68c196e038bd
chriswebdv <chriswebdv@gmail.com>
2024-08-27 15:17:01 -0600

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -204,3 +204,10 @@ function sequentialSizes(val) {
 
 sequentialSizes(1);
 
+// Array Map Function
+
+
+const arr = [1, 2, 3, 4, 5];
+const mappedArr = arr.map((num) => num + 1);
+console.log(mappedArr); // Outputs [2, 3, 4, 5, 6]
+
