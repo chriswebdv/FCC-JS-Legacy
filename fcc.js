74a982c7c0b02cf8371f690429452fc3985bf6d4
chriswebdv <chriswebdv@gmail.com>
2024-08-28 06:22:30 -0600

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -239,3 +239,24 @@ function chainToSwitch(val) {
 
 chainToSwitch(7);
 
+// change this function
+
+function isLess(a, b) {
+  // Only change code below this line
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+  // Only change code above this line
+}
+
+isLess(10, 15);
+
+function isLess(a, b) {
+  // Only change code below this line
+  return a <= b;
+  // Only change code above this line
+}
+
+isLess(10, 15);
