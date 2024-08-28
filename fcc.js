f7c90525f0667fea7263f92574386a2ce7100f7a
chriswebdv <chriswebdv@gmail.com>
2024-08-28 06:45:05 -0600

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -276,3 +276,37 @@ function abTest(a, b) {
 }
 
 abTest(2,2);
+
+// Count cards
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      return count.toString() + " Bet";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      return count.toString() + " Hold"
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count -= 1
+      return count.toString() + " Hold"
+      break;
+  }
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
