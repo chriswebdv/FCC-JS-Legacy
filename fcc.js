e9b11897d94b91c0de0620e81e96b17517927f38
chriswebdv <chriswebdv@gmail.com>
2024-08-28 06:18:21 -0600

    change if/ifelse to switch

--- fcc.js
+++ fcc.js
@@ -205,9 +205,37 @@ function sequentialSizes(val) {
 sequentialSizes(1);
 
 // Array Map Function
-
-
 const arr = [1, 2, 3, 4, 5];
 const mappedArr = arr.map((num) => num + 1);
 console.log(mappedArr); // Outputs [2, 3, 4, 5, 6]
+
+// change if/ifelse to switch
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val) {
+    case "bob":
+      answer = "Marley";
+      break;
+    case 42:
+      answer = "The Answer";
+      break;
+    case 1:
+      answer = "There is no #1";
+      break;
+    case 99:
+      answer = "Missed me by this much!";
+      break;
+    case 7:
+      answer = "Ate Nine";
+      break;
+    default:
+      answer = ""; 
+  }
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
 
