1bf0face9c79714cfa8fa77fe0fc509dde44b9a6
chriswebdv <chriswebdv@gmail.com>
2024-08-29 06:40:57 -0600

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -321,7 +321,7 @@ const myDog = {
   // Only change code above this line
 };
 
-// Prarctice dot notation
+// Practice dot notation
 
 // Setup
 const testObj = {
@@ -333,3 +333,20 @@ const testObj = {
 // Only change code below this line
 const hatValue = testObj.hat;      // Change this line
 const shirtValue = testObj.shirt;    // Change this line
+
+// Practice using bracket notation
+
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];   // Change this line
+const drinkValue = testObj["the drink"];    // Change this line
+
+//
+
+
