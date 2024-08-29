77c29ee0682e7ac40ebe81f2779b04a25067f7af
chriswebdv <chriswebdv@gmail.com>
2024-08-29 06:30:03 -0600

    update function and remove one of the cases

--- fcc.js
+++ fcc.js
@@ -289,22 +289,20 @@ function cc(card) {
     case 4:
     case 5:
     case 6:
-      count += 1;
-      return count.toString() + " Bet";
-      break;
-    case 7:
-    case 8:
-    case 9:
-      return count.toString() + " Hold"
+      count++;
       break;
     case 10:
     case "J":
     case "Q":
     case "K":
     case "A":
-      count -= 1
-      return count.toString() + " Hold"
+      count--;
       break;
+  }
+  if (count > 0) {
+    return count + " Bet"
+  } else {
+    return count + " Hold"
   }
   // Only change code above this line
 }
