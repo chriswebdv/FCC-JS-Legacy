7097e44243c914838f814a3c3d1265aa57fb28fd
chriswebdv <chriswebdv@gmail.com>
2024-09-02 12:38:49 -0600

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -475,3 +475,28 @@ const myStorage = {
 };
 
 const gloveBoxContents = myStorage.car.inside["glove box"];
+
+// second example
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
+
+//  
+
