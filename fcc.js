45355de60e7307be904904533d3887b63afecf88
chriswebdv <chriswebdv@gmail.com>
2024-09-03 18:32:11 -0600

    Use while loop to log out numbers in descending order

--- fcc.js
+++ fcc.js
@@ -498,5 +498,14 @@ const myPlants = [
 
 const secondTree = myPlants[1].list[1];
 
-//  
+//  Use while loop to log out numbers in descending order
+// Setup
+const myArray = [];
 
+// Only change code below this line
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  i--;
+}
