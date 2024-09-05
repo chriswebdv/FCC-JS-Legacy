4f9696b01873b87e3b6100fcab38753c7517f410
chriswebdv <chriswebdv@gmail.com>
2024-09-04 18:43:49 -0600

    change variable in second for loop to j

--- fcc.js
+++ fcc.js
@@ -559,7 +559,7 @@ function multiplyAll(arr) {
   let product = 1;
   // Only change code below this line
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; i < arr[i].length; j++) {
+    for (let j = 0; j < arr[i].length; j++) {
       product *= arr[i][j];
     }
   }
