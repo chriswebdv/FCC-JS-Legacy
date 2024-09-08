2d4ddd1edb65a646f015068d7496aae33010f8a0
chriswebdv <chriswebdv@gmail.com>
2024-09-08 17:34:03 -0600

    Update codesmith.js

--- codesmith.js
+++ codesmith.js
@@ -1,1 +1,7 @@
+// function
+function randomWholeNum() {
+  return Math.floor(Math.random() * 10);;
+}
 
+console.log(typeof addTwo); // should log: 'function'
+console.log(addTwo(10)); // should log: 12
