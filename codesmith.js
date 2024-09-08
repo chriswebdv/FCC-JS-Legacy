fb4deafb9da7c943260d38fb0427ed916c19fe21
chriswebdv <chriswebdv@gmail.com>
2024-09-08 17:36:13 -0600

    write function that concats an "s" to the end of a string

--- codesmith.js
+++ codesmith.js
@@ -5,3 +5,14 @@ function randomWholeNum() {
 
 console.log(typeof addTwo); // should log: 'function'
 console.log(addTwo(10)); // should log: 12
+
+// write function that concasts an "s" to the end of a string
+// ADD CODE HERE
+
+function addS(str) {
+  return str + "s"
+}
+
+// Uncomment these to check your work!
+console.log(typeof addS); // should log: 'function'
+console.log(addS('cat')); // should log: 'cats'
