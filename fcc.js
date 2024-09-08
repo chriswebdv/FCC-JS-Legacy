7c3a78c8717de50c69c98488872005a3effa9526
chriswebdv <chriswebdv@gmail.com>
2024-09-08 13:17:29 -0600

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -594,3 +594,33 @@ function sum(arr, n) {
   // Only change code above this line
 }
 
+// 
+// Setup
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+//
+
