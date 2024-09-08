c1fdfc81d82fe9a880da6c488bd7981d6829f510
chriswebdv <chriswebdv@gmail.com>
2024-09-08 17:10:47 -0600

    Update fcc.js

--- fcc.js
+++ fcc.js
@@ -622,17 +622,19 @@ const contacts = [
     likes: ["JavaScript", "Gaming", "Foxes"],
   },
 ];
-//
+
 function lookUpProfile(name, prop) {
   // Only change code below this line
   for (let i = 0; i < contacts.length; i++) {
-    if (contacts.firstName !== name) {
-      return "No such contact";
-    } else if (!contacts[i].hasOwnProperty(prop)) {
-      return "No such property";
-    } else {
-      return prop;
+    if (contacts[i].firstName === name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop];
+      } else {
+      return "No such property"
+      }
+    } 
     }
+  return "No such contact";
 
   // Only change code above this line
 }
